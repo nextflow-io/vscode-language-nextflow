08b43a5b2a89d20e664fe1d45175e4c344d88808
Phil Ewels <phil.ewels@seqera.io>
2024-11-28 14:09:27 +0100

    Mermaid DAG: Theme with nicer colours

--- src/main/ts/extension.ts
+++ src/main/ts/extension.ts
@@ -6,12 +6,10 @@ import {
   LanguageClientOptions,
   Executable,
 } from "vscode-languageclient/node";
-
 const LABEL_RELOAD_WINDOW = "Reload Window";
 let extensionContext: vscode.ExtensionContext | null = null;
 let languageClient: LanguageClient | null = null;
 let javaPath: string | null = null;
-
 function startLanguageServer() {
   vscode.window.withProgress(
     { location: vscode.ProgressLocation.Window },
@@ -88,7 +86,6 @@ function startLanguageServer() {
     }
   );
 }
-
 async function previewDag(uri: string, name?: string) {
   const content: string = await vscode.commands.executeCommand("nextflow.server.previewDag", uri, name);
   if (!content) {
@@ -105,12 +102,69 @@ async function previewDag(uri: string, name?: string) {
     }
   );
   panel.webview.html = `
-    <html>
-    <head>
+  <html>
+  <head>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-    </head>
-    <body>
-    <pre class="mermaid" style="text-align: center;">
+    <style>
+      :root {
+        --mm-font-family: var(--vscode-font-family, sans-serif);
+        --mm-container-square-fill: var(--vscode-sideBarTitle-background, #BABCBD22);
+        --mm-container-square-stroke: var(--vscode-tab-lastPinnedBorder, #DDDEDE);
+        --mm-container-title: var(--vscode-sideBarTitle-foreground, #160F26);
+        --mm-node-text: var(--vscode-tab-activeForeground, #160F26);
+        --mm-node-fill: var(--vscode-textCodeBlock-background, #B6ECE2);
+        --mm-node-fill-hover: var(--vscode-textBlockQuote-background, #86E0CE);
+        --mm-node-border: var(--vscode-checkbox-selectBorder, #065647);
+        --mm-connector-lines: var(--vscode-tab-unfocusedActiveModifiedBorder, #545555);
+      }
+      .mermaid {
+        text-align: center;
+      }
+      .mermaid .nodeLabel, .mermaid .label {
+        font-family: var(--mm-font-family) !important;
+      }
+      .mermaid .nodeLabel {
+        font-family: var(--mm-font-family) !important;
+      }
+      .mermaid .cluster-label .nodeLabel {
+        font-weight: 600;
+        font-size: 18px;
+      }
+      /* Workflow / take / emit rectangles */
+      .mermaid .cluster rect {
+        fill: var(--mm-container-square-fill) !important;
+        stroke: var(--mm-container-square-stroke) !important;
+        stroke-width: 2px !important;
+      }
+      .mermaid .cluster span {
+        color: var(--mm-container-title) !important;
+      }
+      /* All node rectangles */
+      .mermaid .node rect {
+        fill: var(--mm-node-fill) !important;
+        stroke: var(--mm-node-border) !important;
+        stroke-width: 3px !important;
+        transition: fill .15s ease !important;
+      }
+      .mermaid a .node:hover rect {
+        fill: var(--mm-node-fill-hover) !important;
+      }
+      .mermaid .nodeLabel {
+        fill: var(--mm-node-text) !important;
+        color: var(--mm-node-text) !important;
+      }
+      /* Connector lines */
+      .mermaid .flowchart-link {
+        stroke: var(--mm-connector-lines) !important;
+      }
+      .mermaid .marker {
+        fill: var(--mm-connector-lines) !important;
+        stroke: var(--mm-connector-lines) !important;
+      }
+    </style>
+  </head>
+  <body>
+  <pre class="mermaid">
     ${content.replace(/href "([^"]+)"/g, 'href "command:nextflow.openFileFromWebview?%5B%22$1%22%5D"')}
     </pre>
     <script type="module">
@@ -120,8 +174,8 @@ async function previewDag(uri: string, name?: string) {
     </body>
     </html>
   `;
+  vscode.window.showErrorMessage(panel.webview.html);
 }
-
 function restartLanguageServer() {
   if (!languageClient) {
     startLanguageServer();
@@ -144,21 +198,18 @@ function restartLanguageServer() {
     }
   );
 }
-
 function stopLanguageServer() {
   if (!languageClient) {
     return;
   }
   languageClient.stop()
 }
-
 function onDidChangeConfiguration(event: vscode.ConfigurationChangeEvent) {
   if (event.affectsConfiguration("nextflow.java.home")) {
     javaPath = findJava();
     restartLanguageServer();
   }
 }
-
 export function activate(context: vscode.ExtensionContext) {
   extensionContext = context;
   javaPath = findJava();
@@ -176,7 +227,6 @@ export function activate(context: vscode.ExtensionContext) {
   });
   startLanguageServer();
 }
-
 export function deactivate() {
   extensionContext = null;
 }
