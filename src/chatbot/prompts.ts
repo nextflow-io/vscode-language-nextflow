5c477b2ed019e75b339d843750a52a273d7429c3
Sasha Dagayev <odagayev@gmail.com>
2025-02-05 10:36:29 -0500

    install instructions

--- src/chatbot/prompts.ts
+++ src/chatbot/prompts.ts
@@ -359,6 +359,106 @@ Encourage the use of the nf-test DSL and proper assertion methods from the docum
 
 ─────────────────────────────────────────────  
 **Getting Started with nf-test**  
+_____________________________________________
+
+Installation instructions:
+
+export const nfTestInstallationInstructions = 
+nf-test has the same requirements as Nextflow and can be used on POSIX compatible systems like Linux or OS X. You can install nf-test using the following command:
+
+\`\`\`bash
+curl -fsSL https://get.nf-test.com | bash
+\`\`\`
+
+If you don't have curl installed, you could use wget:
+
+\`\`\`bash
+wget -qO- https://get.nf-test.com | bash
+\`\`\`
+
+It will create the nf-test executable file in the current directory. Optionally, move the nf-test file to a directory accessible by your $PATH variable.
+
+Verify installation
+Test the installation with the following command:
+
+\`\`\`bash
+nf-test version
+\`\`\`
+
+You should see something like this:
+
+\`\`\`
+🚀 nf-test 0.5.0
+https://code.askimed.com/nf-test
+(c) 2021 -2022 Lukas Forer and Sebastian Schoenherr
+
+Nextflow Runtime:
+
+      N E X T F L O W
+      version 21.10.6 build 5660
+      created 21-12-2021 16:55 UTC (17:55 CEST)
+      cite doi:10.1038/nbt.3820
+      http://nextflow.io
+\`\`\`
+
+Now you are ready to write your first testcase.
+
+Install a specific version
+If you want to install a specific version pass it to the install script as so:
+
+\`\`\`bash
+curl -fsSL https://get.nf-test.com | bash -s 0.7.0
+\`\`\`
+
+Manual installation
+All releases are also available on Github.
+
+Nextflow Binary not found?
+If you get an error message like this, then nf-test was not able to detect your Nextflow installation.
+
+\`\`\`
+🚀 nf-test 0.5.0
+https://code.askimed.com/nf-test
+(c) 2021 -2022 Lukas Forer and Sebastian Schoenherr
+
+Nextflow Runtime:
+Error: Nextflow Binary not found. Please check if Nextflow is in a directory accessible by your $PATH variable or set $NEXTFLOW_HOME.
+\`\`\`
+
+To solve this issue you have two possibilites:
+
+1. Move your Nextflow binary to a directory accessible by your $PATH variable.
+2. Set the environment variable NEXTFLOW_HOME to the directory that contains the Nextflow binary.
+
+Updating
+To update an existing nf-test installation to the latest version, run the following command:
+
+\`\`\`bash
+nf-test update
+\`\`\`
+
+Compiling from source
+To compile nf-test from source you shall have maven installed. This will produce a \`nf-test/target/nf-test.jar\` file.
+
+\`\`\`bash
+git clone git@github.com:askimed/nf-test.git
+cd nf-test
+mvn install
+\`\`\`
+
+To use the newly compiled \`nf-test.jar\`, update the nf-test bash script that is on your PATH to point to the new .jar file. First locate it with \`which nf-test\`, and then modify \`APP_HOME\` and \`APP_JAR\` vars at the top:
+
+\`\`\`bash
+#!/bin/bash
+APP_HOME="/PATH/TO/nf-test/target/"
+APP_JAR="nf-test.jar"
+APP_UPDATE_URL="https://code.askimed.com/install/nf-test"
+...
+\`\`\`
+
+
+─────────────────────────────────────────────  
+**Getting Started with nf-test**  
 ─────────────────────────────────────────────  
 - **Installation & Setup:**  
   - Ensure nf-test is installed properly on the user’s computer.
