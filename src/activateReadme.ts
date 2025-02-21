c77e8863e9b3b0d551584cc3463d13cf36ee0bc3
Jake Broughton <jake.broughton@seqera.io>
2025-02-21 14:30:17 +0100

    Only show on initial installation

--- src/activateReadme.ts
+++ src/activateReadme.ts
@@ -2,14 +2,7 @@ import * as vscode from "vscode";
 import showPage from "./utils/showPage";
 
 export function activateReadme(context: vscode.ExtensionContext) {
-  // Show on activation
-  if (vscode.env.appName.includes("Cursor")) {
-    showPage("readme-cursor.md");
-  } else {
-    showPage("readme-vscode.md");
-  }
-
-  // Add command to show the readme
+  // Add command to show readme
   const showReadme = vscode.commands.registerCommand(
     "nextflow.showReadme",
     () => {
@@ -22,6 +15,20 @@ export function activateReadme(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(showReadme);
+
+  // Show readme on installation of new version
+
+  const versionKey = "nextflow.version";
+  const extension = vscode.extensions.getExtension("nextflow.nextflow");
+  const currentVersion = extension?.packageJSON.version ?? "unknown";
+  const storedVersion = context.globalState.get<string>(versionKey, "");
+  const isCursor = vscode.env.appName.includes("Cursor");
+  let readme = "readme-vscode.md";
+  if (isCursor) readme = "readme-cursor.md";
+
+  if (currentVersion === storedVersion) return;
+  context.globalState.update(versionKey, currentVersion);
+  showPage(readme);
 }
 
 export default { activateReadme };
