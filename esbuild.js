c2136bd4f6d9a8dba0200fdd8d38573c6d5b917e
Jake Broughton <jake.broughton@seqera.io>
2025-02-21 17:03:10 +0100

    Fix build size

--- esbuild.js
+++ esbuild.js
@@ -1,25 +1,25 @@
 const { build } = require('esbuild');
 const { copy } = require('esbuild-plugin-copy');
+const path = require('path');
 
 const production = process.argv.includes('--production');
 
 async function main() {
   const files = {
-    'images/**': './images',
-    'snippets/**': './snippets',
-    'syntaxes/**': './syntaxes',
-    'CHANGELOG.md': './CHANGELOG.md',
-    'LICENSE.md': './LICENSE.md',
-    'README.md': './README.md',
-    'language-configuration.json': './language-configuration.json',
-    'package.json': './package.json',
-    'language-server/build/libs/language-server-all.jar': 'build/bin',
-    'node_modules/mermaid/dist/mermaid.min.js': 'build/media',
+    'images/**': 'images',
+    'snippets/**': 'snippets',
+    'syntaxes/**': 'syntaxes',
+    'CHANGELOG.md': 'CHANGELOG.md',
+    'LICENSE.md': 'LICENSE.md',
+    'README.md': 'README.md',
+    'language-configuration.json': 'language-configuration.json',
+    'package.json': 'package.json',
+    'language-server/build/libs/language-server-all.jar': 'bin/language-server-all.jar',
+    'node_modules/mermaid/dist/mermaid.min.js': 'media/mermaid.min.js'
   };
+
   await build({
-    entryPoints: [
-      'src/extension.ts'
-    ],
+    entryPoints: ['src/extension.ts'],
     bundle: true,
     format: 'cjs',
     minify: production,
@@ -31,11 +31,13 @@ async function main() {
     logLevel: 'silent',
     plugins: [
       copy({
-        assets: Object.entries(files).map(([from, to]) => {
-          return { from, to };
-        }),
-      }),
-    ],
+        resolveFrom: 'cwd',
+        assets: Object.entries(files).map(([from, to]) => ({
+          from,
+          to: path.join('build', to)
+        }))
+      })
+    ]
   });
 }
 
