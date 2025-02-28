eac3161a980040dcaa3a08cdd4634f0e314d9ad8
Jake Broughton <jake.broughton@seqera.io>
2025-02-28 19:33:15 +0100

    Cleanup

--- src/webview/index.ts
+++ src/webview/index.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
-import getNonce from "./utils/getNonce";
 import updateRefs from "./utils/updateRefs";
 
 interface PipelineFile {
