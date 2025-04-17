2bf3678126aeca3df24fb556762b643ade77d38a
Jake Broughton <jake.broughton@seqera.io>
2025-04-17 17:51:51 +0200

    Link history items

--- webview-ui/src/Layout/Cloud/Workspace/RunHistory/index.tsx
+++ webview-ui/src/Layout/Cloud/Workspace/RunHistory/index.tsx
@@ -1,15 +1,28 @@
 import { useTowerContext } from "../../../../Context";
-
+import { getRunHistoryURL } from "./utils";
+import Button from "../../../../components/Button";
 const RunHistory = () => {
-  const { history } = useTowerContext();
+  const { workspaces, selectedWorkspace, history } = useTowerContext();
+
+  const workspace = workspaces?.find(
+    (ws) => ws.workspaceId === selectedWorkspace
+  );
 
   return (
     <div>
-      <h4 className="mt-0">Run History</h4>
+      <h4>Run History</h4>
       {!!history?.length && (
         <>
           {history.map((workflow) => (
-            <div key={workflow.id}>{workflow.runName}</div>
+            <Button
+              href={getRunHistoryURL(workspace, workflow)}
+              key={workflow.id}
+              fullWidth
+              alt
+              className="mb-1"
+            >
+              {workflow.runName}
+            </Button>
           ))}
         </>
       )}
