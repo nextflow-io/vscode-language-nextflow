8f6ecb83f19463a9d7af4dd0b1fbb4429eb5d009
Ben Sherman <bentshermann@gmail.com>
2025-05-01 12:57:21 -0500

    Increase retries on layout init
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- webview-ui/src/Layout/index.tsx
+++ webview-ui/src/Layout/index.tsx
@@ -12,11 +12,11 @@ const Layout = () => {
     // Hacky fix for empty file state we get sometimes
     // TODO: find out why this is happening
     if (viewID !== "project") return;
-    if (!nodes.length && retryCount < 2) {
+    if (!nodes.length && retryCount <= 2) {
       timeoutRef.current = setTimeout(() => {
         refresh();
         setRetryCount((prev) => prev + 1);
-      }, 1000);
+      }, 2000);
     } else {
       const ref = timeoutRef.current;
       if (ref) clearTimeout(ref);
