8578defc9b2f97e099b6fcc1cee4e176aee31632
Jake Broughton <jake.broughton@seqera.io>
2025-05-19 16:27:22 +0200

    Partially working state

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const SEQERA_PLATFORM_URL = `https://cloud.seqera.io`;
+// export const SEQERA_PLATFORM_URL = `https://cloud.seqera.io`;
+export const SEQERA_PLATFORM_URL = `https://pr-8246.dev-tower.net`;
 export const SEQERA_API_URL = `${SEQERA_PLATFORM_URL}/api`;
 export const SEQERA_HUB_API_URL = `https://hub.seqera.io`;
 export const SEQERA_INTERN_API_URL = `https://intern.seqera.io`;
--- src/webview/WebviewProvider/lib/platform/utils/fetchUserInfo.ts
+++ src/webview/WebviewProvider/lib/platform/utils/fetchUserInfo.ts
@@ -3,13 +3,14 @@ import type { UserInfoResponse } from "../types";
 
 const fetchUserInfo = async (token: string): Promise<UserInfoResponse> => {
   if (!token) return { message: "No token found" } as UserInfoResponse;
+  console.log("🟢 fetchUserInfo", token);
   try {
     const response = await fetch(`${SEQERA_API_URL}/user-info`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
     });
-    console.log("🟣 fetchUserInfo", response.status);
+    console.log("🟣 fetchUserInfo", response);
     if (response.status === 401) {
       throw new Error("Unauthorized");
     }
