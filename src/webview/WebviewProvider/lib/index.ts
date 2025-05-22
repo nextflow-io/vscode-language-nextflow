0e8aa1838cbc52cb8dfa90b862e0c449d5649b92
Jake Broughton <jake.broughton@seqera.io>
2025-05-22 16:00:12 +0200

    Cleanup

--- src/webview/WebviewProvider/lib/index.ts
+++ src/webview/WebviewProvider/lib/index.ts
@@ -1,4 +1,5 @@
 export { default as fetchPlatformData } from "./platform/fetchPlatformData";
+export { default as clearPlatformData } from "./platform/clearPlatformData";
 export { default as getAuthState } from "./platform/getAuthState";
 export * from "./platform/utils";
 
