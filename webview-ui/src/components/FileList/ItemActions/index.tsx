5a37bd1bcb2bd14fa044dd392e63f6058a0266bf
dana-seqera <dana.kim@seqera.io>
2025-05-27 16:45:11 -0500

    Add color to wave/nf-test icons (#133)

--- webview-ui/src/components/FileList/ItemActions/index.tsx
+++ webview-ui/src/components/FileList/ItemActions/index.tsx
@@ -50,7 +50,7 @@ const ItemActions: React.FC<Props> = ({ node }) => {
             !!node.test && openFile(node.test.path, node.test.line)
           }
         >
-          <i className={clsx("codicon", "codicon-beaker", styles.actionIcon)} />
+          <i className={clsx("codicon", "codicon-beaker", styles.actionIcon, styles.actionGo)} />
         </button>
       ) : (
         <button
